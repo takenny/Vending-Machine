9dec89949694b0d0514ab16208ca01f803617d88
Kenny Ta <kenny_ta@yahoo.com>
2021-12-17 19:46:15 -0800

    Update main.c
    Updated with code from yestesrday

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+//might change this idk if store globally is okay
 float userAmount = 0;
 float quarter = 0.25;
 float dime = 0.10;
 float nickel = 0.05;
 float penny = 0.01;
 
-void insert_Money(); //unsure if need this method ? yet probably do
+float insert_Money(); //unsure if need this method ? yet probably do
 void display_Selections();
 float calculateChange();
 
@@ -16,6 +17,10 @@ int main(){
   float money = 0;
 
   printf("Welcome to the Vending Machine!\n\n");
+  //print if want to make purchase or add item to the thing
+    //if make purchase, proceed as normal with insert money method
+    //if add item, then ask user what item and how much the item is to add. <- these things might need to add to uml
+
   //inseart money now
   printf("Please insert your money.\n");
   scanf("%f", &money);
@@ -25,11 +30,12 @@ int main(){
 
     display_Selections();
     printf("Please select an Option\n");
-    scanf("%d", &option);
+    scanf("%d", &option); //check if broken then need to fix ?? when insert oiption  = yes?
 
     switch(option)
     {
-      case 1: break;
+      case 1: calculateChange(money, option);
+        break;
       case 2: break;
       case 3: break;
       case 4: break;
@@ -51,8 +57,27 @@ int main(){
   return 0;
 }
 
-void insert_Money()
+/*
+    Function inserts money into machine asking user for choices.
+    Then inputs that into the variable for userMoney.
+
+*/
+float insert_Money()
 {
+    float totalInput = 0;
+    float pennies, nickels, dimes, quarters;
+    printf("How many pennies are you inserting?\n");
+    scanf("%f", &pennies);
+    printf("How many nickels are you inserting? \n");
+    scanf("%f", &nickels);
+    printf("How many dimes?\n");
+    scanf("%f", &dimes);
+    printf("Quarters?\n");
+    scanf("%f", &quarters);
+
+    totalInput = pennies*0.01 + nickels*0.05 + dimes*0.10 + quarters*0.25;
+
+    return totalInput;
 
 }
 
@@ -69,7 +94,25 @@ void display_Selections()
   printf("\n6. Cancel Selection\n");
 }
 
-float calculateChange()
+/*
+    Function calculates the change from the transaction and returns change to user
+    Function will take in 2 params, 1 from selection chosen, another from the amount the user inputs
+*/
+float calculateChange(/*moneyUser, and userChoice*/)
 {
+    float change = 0;
+    float cheetos, gummy_Bears = 0.50;
+    float lays = 0.75;
+    float ritz_Crackers = 1.00;
+    float oreos = 1.50;
+
+    //do calcluation for the choices added etc.
+
+    return change;
+}
+
+void addOptions()
+{
+    /*do this function last. might mess up code in the beginning. */
 
 }
